3798b6f841910c1c7afdda511ca6a5bfd6a32494
anmasi <simone.dinuovo@finconsgroup.com>
2015-12-16 10:07:04 +0100

    remove bbcode and fixed bug with deeper json

--- tasks/multi_lang_site_generator.js
+++ tasks/multi_lang_site_generator.js
@@ -5,13 +5,12 @@ module.exports = function (grunt) {
     var _  = require('lodash'),
         fs = require('fs');
 
-    grunt.registerMultiTask('multi_lang_site_generator', 'Create multiple translated sites based on templates and vocab json objects.', function () {
+    grunt.registerMultiTask('atlas_multi_lang', 'Create multiple translated sites based on templates and vocab json objects.', function () {
 
         var options = this.options({
                 vocabs :            [],
                 data:               {},
                 output_directory:   '',
-                template_directory: '',
                 sub_templates:      '',
                 vocab_directory:    ''
             }),
@@ -22,6 +21,7 @@ module.exports = function (grunt) {
 
         languages.forEach(function (lng) {
             files.forEach(function (f) {
+                console.log(f);
                 generate_output_file(grunt, lng, f, options);
             });
         });
@@ -40,7 +40,6 @@ module.exports = function (grunt) {
     }
 
     function get_list_of_languages (vocabs, vocab_directory) {
-        var list_of_languages = [];
         if (all_vocabs_should_be_processed(vocabs)) {
             return _.map(fs.readdirSync(vocab_directory), function (fileName) {
                 return fileName.replace('.json', '');
@@ -77,10 +76,9 @@ module.exports = function (grunt) {
             special_variables = {
                 vocab_dir: lng
             },
-            parsed_vocab_data = replace_bb_code_with_markup_in(vocab_data),
-            data = _.merge(options.data, parsed_vocab_data, special_variables),
+            data = _.merge(options.data, vocab_data, special_variables),
             src  = _.template(
-                grunt.file.read(options.template_directory + f.src[0]),
+                grunt.file.read(f.src),
                 data,
                 define_the_imports_keyword(options, data, function () {
                     return define_the_imports_keyword(options, data);
@@ -104,7 +102,6 @@ module.exports = function (grunt) {
     }
 
     function add_forward_slash_to_end_of_dir_paths (options) {
-        options.template_directory = do_the_forward_slash_adding(options.template_directory);
         options.vocab_directory    = do_the_forward_slash_adding(options.vocab_directory);
         options.output_directory   = do_the_forward_slash_adding(options.output_directory);
     }
@@ -119,34 +116,6 @@ module.exports = function (grunt) {
         return directory;
     }
 
-    function replace_bb_code_with_markup_in (vocabs_data){
-        var parsed_vocabs = vocabs_data,
-            vocab;
-
-        for (vocab in vocabs_data) {
-            parsed_vocabs[vocab] = parse_vocab_item(parsed_vocabs[vocab]);
-        }
-
-        return parsed_vocabs;
-    }
-
-    function parse_vocab_item (vocabItem) {
-        /* Replaces {B} and {/B} with <strong> and </strong> */
-        vocabItem = vocabItem.replace(/\{B\}/gi, '<strong>').replace(/\{\/B\}/gi, '</strong>');
-
-        /* Replaces {P} and {/P} with <p> and </p> */
-        vocabItem = vocabItem.replace(/\{P\}/gi, '<p>').replace(/\{\/P\}/gi, '</p>');
-
-        /* matchs {URL=X}Y{/URL} to generate a href tag */
-        var urlArray = vocabItem.match(/\{URL=(.*)\}(.*)\{\/URL\}/i);
-        if (urlArray !== null){
-            var hrefTag = '<a href="' + urlArray[1] + '" target="_top">' + urlArray[2] + '</a>';
-            vocabItem = vocabItem.replace(urlArray[0], hrefTag);
-        }
-
-        return vocabItem;
-    }
-
     function define_the_imports_keyword (options, data, callback) {
         callback = callback || function () {};
         return {
@@ -155,7 +124,7 @@ module.exports = function (grunt) {
                     params = params || {};
                     var include_data = _.merge(data, params);
                     return _.template(
-                        fs.readFileSync(options.template_directory + tmpl).toString(),
+                        fs.readFileSync(tmpl).toString(),
                         include_data,
                         callback()
                     );
